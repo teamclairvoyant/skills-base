ebd4374f8313a25f28f7d9c6f657305090b1540f
Sirisha <sirisha.boyena@clairvoyantsoft.com>
2023-01-11 21:01:30 +0530

    added PasswordUtil class with encode and matches methods

--- ClariseMain/src/main/java/com/clairvoyant/clarise/util/PasswordUtil.java
+++ ClariseMain/src/main/java/com/clairvoyant/clarise/util/PasswordUtil.java
@@ -1,16 +1,15 @@
 package com.clairvoyant.clarise.util;
 
-import java.util.Base64;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordUtil {
     public static String encode(String password) {
-        Base64.Encoder encoder = Base64.getEncoder();
-        return encoder.encodeToString(password.getBytes());
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
     }
 
-    public static String decode(String encryptedPassword) {
-        Base64.Decoder decoder = Base64.getDecoder();
-        String password = new String(decoder.decode(encryptedPassword));
-        return password;
+    public static boolean matches(String rawPassword, String encodedPassword) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.matches(rawPassword, encodedPassword);
     }
 }

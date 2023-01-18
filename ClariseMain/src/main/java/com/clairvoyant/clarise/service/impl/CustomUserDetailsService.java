c176123a16c6fb87aad922902d1e43fa6e7ccf64
Srinivasulu Kommu <kommu.srinivasulu@clairvoyantsoft.com>
2023-01-18 16:32:02 +0530

    custom user details service changes

--- ClariseMain/src/main/java/com/clairvoyant/clarise/service/impl/CustomUserDetailsService.java
+++ ClariseMain/src/main/java/com/clairvoyant/clarise/service/impl/CustomUserDetailsService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.clairvoyant.clarise.model.User;
 import com.clairvoyant.clarise.repository.UserRepository;
-import com.clairvoyant.clarise.utility.CustomUserDetails;
+import com.clairvoyant.clarise.util.CustomUserDetails;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {

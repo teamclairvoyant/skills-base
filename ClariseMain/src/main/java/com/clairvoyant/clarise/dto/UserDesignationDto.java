b6369835077c05caa596a6b99617c01b6bb84f29
nivrutti1234 <121482323+nivrutti1234@users.noreply.github.com>
2023-01-20 14:13:48 +0530

    SM-49 User crud apis (#25)
    * User crud apis
    
    * minor changes done
    
    * Made changes in imports

---
+++ ClariseMain/src/main/java/com/clairvoyant/clarise/dto/UserDesignationDto.java
@@ -0,0 +1,18 @@
+package com.clairvoyant.clarise.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDesignationDto {
+
+    private String userId;
+
+    private String designationId;
+
+}
---
+++ ClariseMain/src/main/java/com/clairvoyant/clarise/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.clairvoyant.clarise.repository;
+
+import com.clairvoyant.clarise.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,String> {
+}

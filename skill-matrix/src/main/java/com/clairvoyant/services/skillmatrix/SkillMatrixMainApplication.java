eb735e5a5b773db50ec0b2d256f544eebf44bd94
Sirisha <sirisha.boyena@clairvoyantsoft.com>
2023-02-04 21:45:39 +0530

    SM-62 added modelMapper bean

--- skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/SkillMatrixMainApplication.java
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/SkillMatrixMainApplication.java
@@ -1,10 +1,16 @@
 package com.clairvoyant.services.skillmatrix;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SkillMatrixMainApplication {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
     public static void main(String[] args) {
         SpringApplication.run(SkillMatrixMainApplication.class, args);
     }

acea736e51143cc67d630f2f50a7ec631a258a6f
Sirisha <sirisha.boyena@clairvoyantsoft.com>
2023-02-06 13:43:21 +0530

    SM-62 moved the bean configuration to separate configuration class

--- skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/SkillMatrixMainApplication.java
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/SkillMatrixMainApplication.java
@@ -1,16 +1,10 @@
 package com.clairvoyant.services.skillmatrix;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SkillMatrixMainApplication {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
     public static void main(String[] args) {
         SpringApplication.run(SkillMatrixMainApplication.class, args);
     }

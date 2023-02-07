e6c8442abd5781036bc77e41295a21550fbd2b4a
Sirisha <sirisha.boyena@clairvoyantsoft.com>
2023-02-07 16:27:54 +0530

    SM-65 added DuplicateNameException handling

--- skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/service/impl/SkillServiceImpl.java
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/service/impl/SkillServiceImpl.java
@@ -1,6 +1,8 @@
 package com.clairvoyant.services.skillmatrix.service.impl;
 
 import com.clairvoyant.services.skillmatrix.dto.SkillDto;
+import com.clairvoyant.services.skillmatrix.exceptions.DuplicateNameException;
+import com.clairvoyant.services.skillmatrix.exceptions.ResourceNotFoundException;
 import com.clairvoyant.services.skillmatrix.model.Skill;
 import com.clairvoyant.services.skillmatrix.repository.SkillRepository;
 import com.clairvoyant.services.skillmatrix.service.SkillService;
@@ -10,6 +12,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -31,26 +34,32 @@ public class SkillServiceImpl implements SkillService {
     @Override
     public SkillDto addOrUpdateSkill(SkillDto skillDto) {
         Skill skill = null;
-        if (StringUtils.hasText(skillDto.getId())) {
-            Optional<Skill> dbSkill = skillRepository.findById(skillDto.getId());
-            if (dbSkill.isPresent()) {
-                if (StringUtils.hasLength(skillDto.getSkillName())) {
-                    dbSkill.get().setSkillName(skillDto.getSkillName());
-                }
+        try {
+            if (StringUtils.hasText(skillDto.getId())) {
+                Optional<Skill> dbSkill = skillRepository.findById(skillDto.getId());
+                if (dbSkill.isPresent()) {
+                    if (StringUtils.hasLength(skillDto.getSkillName())) {
+                        dbSkill.get().setSkillName(skillDto.getSkillName());
+                    }
 
-                if (StringUtils.hasLength(skillDto.getDescription())) {
-                    dbSkill.get().setDescription(skillDto.getDescription());
-                }
+                    if (StringUtils.hasLength(skillDto.getDescription())) {
+                        dbSkill.get().setDescription(skillDto.getDescription());
+                    }
 
-                skill = dbSkill.get();
+                    skill = dbSkill.get();
+                } else {
+                    throw new ResourceNotFoundException("Skill with id " + skillDto.getId() + " not found");
+                }
+            } else {
+                skill = modelMapper.map(skillDto, Skill.class);
+                skill.setActive(true);
             }
-        } else {
-            skill = modelMapper.map(skillDto, Skill.class);
-            skill.setActive(true);
-        }
 
-        if (Objects.nonNull(skill)) {
-            skill = skillRepository.save(skill);
+            if (Objects.nonNull(skill)) {
+                skill = skillRepository.save(skill);
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new DuplicateNameException("Skill with name " + skillDto.getSkillName() + " already exists");
         }
 
         return modelMapper.map(skill, SkillDto.class);
@@ -58,7 +67,11 @@ public class SkillServiceImpl implements SkillService {
 
     @Override
     public SkillDto findSkill(String skillId) {
-        return modelMapper.map(skillRepository.findById(skillId).get(), SkillDto.class);
+        Optional<Skill> dbSkill = skillRepository.findById(skillId);
+        if (dbSkill.isEmpty()) {
+            throw new ResourceNotFoundException("Skill with id " + skillId + " not found");
+        }
+        return modelMapper.map(dbSkill.get(), SkillDto.class);
     }
 
     @Override
@@ -68,6 +81,8 @@ public class SkillServiceImpl implements SkillService {
             Skill skill = dbSkill.get();
             skill.setActive(false);
             skillRepository.save(skill);
+        } else {
+            throw new ResourceNotFoundException("Skill with id " + skillId + " not found");
         }
     }
 

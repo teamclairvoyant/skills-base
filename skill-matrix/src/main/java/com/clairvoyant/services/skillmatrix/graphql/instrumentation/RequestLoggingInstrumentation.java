369b685a72de6f92274811e01387207a6d249336
Sanket P Bambalkar <sanket.bambalkar@clairvoyantsoft.com>
2023-02-15 14:34:05 +0530

    SM-58 - Logger

--- skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/graphql/instrumentation/RequestLoggingInstrumentation.java
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/graphql/instrumentation/RequestLoggingInstrumentation.java
@@ -8,11 +8,10 @@ import graphql.execution.instrumentation.parameters.InstrumentationExecutionPara
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Component;
-
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
+import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor

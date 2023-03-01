e3a32b4fd02b96d0e2d32dc4b4a92965dedaafa0
Sanket P Bambalkar <sanket.bambalkar@clairvoyantsoft.com>
2023-03-01 18:36:55 +0530

    SM-98 - Comments Resolved .

--- skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/advice/ExceptionController.java
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/advice/ExceptionController.java
@@ -2,14 +2,14 @@ package com.clairvoyant.services.skillmatrix.advice;
 
 import com.clairvoyant.services.skillmatrix.exceptions.DuplicateNameException;
 import com.clairvoyant.services.skillmatrix.exceptions.ResourceNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionController {

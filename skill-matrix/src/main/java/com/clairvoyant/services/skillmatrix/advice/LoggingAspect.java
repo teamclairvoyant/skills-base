e5b29b3a533b0cc79450142402b4704b28b4f5a0
Sanket P Bambalkar <sanket.bambalkar@clairvoyantsoft.com>
2023-03-01 15:06:39 +0530

    SM-58 - Logs added for Request and Response . Comments Resolved .

--- skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/advice/LoggingAspect.java
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/advice/LoggingAspect.java
@@ -1,60 +1,65 @@
 package com.clairvoyant.services.skillmatrix.advice;
 
 import java.util.Arrays;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    private final Logger log = LogManager.getLogger(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
 
     /**
      * Pointcut that matches all repositories, services and Web REST endpoints.
      */
-    @Pointcut("within(@org.springframework.stereotype.Repository *)"
-            + " || within(@org.springframework.stereotype.Service *)"
-            + " || within(@org.springframework.web.bind.annotation.RestController *)")
+    @Pointcut(" within(@org.springframework.web.bind.annotation.RestController *)")
     public void springBeanPointcut() {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
-
 
     /**
      * Pointcut that matches all Spring beans in the application's main packages.
      */
     @Pointcut("within(com.clairvoyant.services.skillmatrix..*)"
-            + " || within(com.clairvoyant.services.skillmatrix.service..*)"
-            + " || within(com.clairvoyant.services.skillmatrix.controller..*)"
-            + " || within(com.clairvoyant.services.skillmatrix.graphql.resolver.mutation..*)"
-            + " || within(com.clairvoyant.services.skillmatrix.graphql.resolver.query..*)")
+            + " || within(com.clairvoyant.services.skillmatrix.controller..*)")
     public void applicationPackagePointcut() {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
-
-
 
     /**
      * Advice that logs methods throwing exceptions.
      *
      * @param joinPoint join point for advice
-     * @param e exception
+     * @param e         exception
      */
-
     @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-
         log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
     }
+
 
     /**
      * Advice that logs when a method is entered and exited.
@@ -65,16 +70,33 @@ public class LoggingAspect {
      */
     @Around("applicationPackagePointcut() && springBeanPointcut()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (log.isDebugEnabled() && null != request) {
+            Map<String, String> headerMap = new HashMap<>();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                String headerValue = request.getHeader(headerName);
+                headerMap.put(headerName, headerValue);
+            }
+            log.debug("Entered in method : {}.{}() with argument[s] = {}  \n Request Path info : {} \n HTTP Method Type : {} \n Headers Values : {} ",
+                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()),
+                    request.getServletPath(), request.getMethod(), headerMap);
 
-        if (log.isDebugEnabled()) {
-            log.debug("Started Execution: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
         }
         try {
             Object result = joinPoint.proceed();
-            if (log.isDebugEnabled()) {
-                log.debug("Execution Completed: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                        joinPoint.getSignature().getName(),result.toString());
+            if (log.isDebugEnabled() && null != response) {
+                Collection<String> headerNames = response.getHeaderNames();
+                Map<String, String> headerMap = new HashMap<>();
+                Iterator<String> itr = headerNames.iterator();
+                while (itr.hasNext()) {
+                    String headerName = itr.next();
+                    String headerValue = response.getHeader(headerName);
+                    headerMap.put(headerName, headerValue);
+                }
+                log.debug("Exited from method: {}.{}() with result = {} \n Response Status : {} \n Headers Values : {}",
+                        joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), result, response.getStatus(), headerMap);
+
             }
             return result;
         } catch (IllegalArgumentException e) {
@@ -83,4 +105,6 @@ public class LoggingAspect {
             throw e;
         }
     }
+
+
 }

42518e78f723891d1828c7a54f3d5e321abf76ac
Bhavanisnigdha <snigdha.rekapalli@clairvoyantsoft.com>
2023-03-09 19:29:41 +0530

    Resolved checkstyle issues

--- skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/graphql/resolver/query/AssessmentHistoryQueryResolver.java
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/graphql/resolver/query/AssessmentHistoryQueryResolver.java
@@ -1,10 +1,8 @@
 package com.clairvoyant.services.skillmatrix.graphql.resolver.query;
 
 import com.clairvoyant.services.skillmatrix.dto.AssessmentHistoryResponse;
-
 import com.clairvoyant.services.skillmatrix.service.AssessmentHistoryService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +14,7 @@ public class AssessmentHistoryQueryResolver implements GraphQLQueryResolver {
 
 
     public AssessmentHistoryResponse getAssessmentHistory(String userId, String assessmentId) {
-        return assessmentHistoryService.getAssessmentHistory(userId,assessmentId);
+        return assessmentHistoryService.getAssessmentHistory(userId, assessmentId);
     }
 
 }

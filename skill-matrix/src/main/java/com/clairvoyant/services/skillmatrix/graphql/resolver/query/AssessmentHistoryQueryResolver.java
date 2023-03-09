de5176e90630f1665022a747193fb963b1f761ba
Bhavanisnigdha <snigdha.rekapalli@clairvoyantsoft.com>
2023-03-09 19:16:20 +0530

    Grphql changes

---
+++ skill-matrix/src/main/java/com/clairvoyant/services/skillmatrix/graphql/resolver/query/AssessmentHistoryQueryResolver.java
@@ -0,0 +1,22 @@
+package com.clairvoyant.services.skillmatrix.graphql.resolver.query;
+
+import com.clairvoyant.services.skillmatrix.dto.AssessmentHistoryResponse;
+
+import com.clairvoyant.services.skillmatrix.service.AssessmentHistoryService;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AssessmentHistoryQueryResolver implements GraphQLQueryResolver {
+
+    @Autowired
+    private AssessmentHistoryService assessmentHistoryService;
+
+
+    public AssessmentHistoryResponse getAssessmentHistory(String userId, String assessmentId) {
+        return assessmentHistoryService.getAssessmentHistory(userId,assessmentId);
+    }
+
+}
